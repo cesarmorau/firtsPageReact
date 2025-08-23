163ce68a4df0abec2bc81460e85ff1d0593f1b86
Cesar <cesarmora2005c@gmail.com>
2025-08-22 19:52:37 -0500

    Subiendo proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My firts web with REACT</h1>
+      <div className="container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Card.css"
+
+function Card() {
+  return (
+    <div className='Card'>
+        <h2>Tittle of Card</h2>
+        <p>Description of Card</p>
+    </div>
+  )
+}
+
+export default Card
